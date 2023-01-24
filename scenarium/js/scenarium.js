3e1ba408ddcca2526efece91e89196cf1797d992
Michael <Michael@THINKPAD>
2023-01-24 23:48:36 +0100

    fix missing parsed text

--- scenarium/js/scenarium.js
+++ scenarium/js/scenarium.js
@@ -101,12 +101,12 @@ $(document).ready(async function () {
                 else if (current.isElement(sub_element, 'description')) {
                     $("#rules").append('<p class="lead"></p>').find('p:last-child').text(await current.getElement(sub_element, 'description'));
                 }
-                /*else if (await current.isSequence(sub_element)) {
-                    $("#rules").append('<p></p>').find('p:last-child').text(await current.parseSequence(sub_element));
-                }*/
-                else if (await current.isTemplate(sub_element)) {
+                else if (await current.isSequence(sub_element)) {
                     $("#rules").append('<p></p>').find('p:last-child').text(await current.parseSequence(sub_element));
                 }
+                /*else if (await current.isTemplate(sub_element)) {
+                    $("#rules").append('<p></p>').find('p:last-child').text(await current.parseSequence(sub_element));
+                }*/
             }
         }
         console.log('<<< ELEMENT <<<');
