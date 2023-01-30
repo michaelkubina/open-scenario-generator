f8092d4ed602551dc793cbd385abf2a64d0eb8cc
Michael <Michael@THINKPAD>
2023-01-30 22:38:30 +0100

    update names.json

--- scenarium/js/scenarium.js
+++ scenarium/js/scenarium.js
@@ -6,8 +6,9 @@ $(document).ready(async function () {
     let df = new rtng('https://raw.githubusercontent.com/michaelkubina/open-scenario-generator/main/scenarium/games/dwarf_fortress.json');
     let cdda = new rtng('https://raw.githubusercontent.com/michaelkubina/open-scenario-generator/main/scenarium/games/cdda.json');
     let example = new rtng('https://raw.githubusercontent.com/michaelkubina/open-scenario-generator/main/rtng-js/example.json');
+    let names = new rtng('https://raw.githubusercontent.com/michaelkubina/open-scenario-generator/main/rtng-js/names.json');
 
-    current = example;
+    current = names;
 
 
     // test for object
