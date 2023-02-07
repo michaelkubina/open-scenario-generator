f67b24ba8234c78e6ebbe94333de4ac594c1e362
Michael <Michael@THINKPAD>
2023-02-07 01:00:51 +0100

    quickfix typo

--- rtng-js/rtng.js
+++ rtng-js/rtng.js
@@ -51,7 +51,7 @@ class rtng {
         if (this.promise['@external']) {
             for (let item in this.promise['@external']) {
                 let external = await rtng.init(this.promise['@external'][item]);
-                //console.log(external['promise']);
+                console.log(external);
                 //this.external[this.promise['@external'][item]['namespace']] = external;
                 this.promise['@external'][item] = external['promise'];
             }
